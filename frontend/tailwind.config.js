c4deaa0eb891c54a03dea3cc7ebd62ab13692a5b
Malavika Harikumar <malavikaha02@gmail.com>
2023-10-03 13:59:30 +0530

    Styled chat sidebar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'light-cream': '#E8DDD9',
+        'light-brown': '#BDA193',
+        'mindful-gray-80': '#4B3425',
+        'mindful-gray-40': '#BDA193',
+        'mindful-gray-100': '#1F160F',
+        'empathy-orange-10': '#FFF0EB',
+        'empathy-orange-40': '#FE814B',
+      }
     },
   },
   plugins: [],
