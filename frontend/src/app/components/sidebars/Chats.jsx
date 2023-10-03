7a3bb114c402bdf890134d9c90429e5f92324b7f
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2023-10-03 17:33:41 +0530

    Add chats sidebar for the bot

--- frontend/src/app/components/sidebars/Chats.jsx
+++ frontend/src/app/components/sidebars/Chats.jsx
@@ -4,70 +4,70 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 
 function Chats() {
-    const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedItem, setSelectedItem] = useState(null);
 
-    const items = [
-        {
-            name: "Current",
-            count: 12,
-            icon: "/icons/dashboard.svg"
-        },
-        {
-            name: "Bookmarked",
-            count: 12,
-            icon: "/icons/mobile.svg"
-        },
-        {
-            name: "Favourite",
-            count: 12,
-            icon: "/icons/star.svg"
-        },
-        {
-            name: "Trash",
-            count: 12,
-            icon: "/icons/trash.svg"
-        },
-        {
-            name: "Unassigned",
-            count: 12,
-            icon: "/icons/question.svg"
-        }
-    ];
+  const items = [
+    {
+      name: "Current",
+      count: 12,
+      icon: "/icons/dashboard.svg"
+    },
+    {
+      name: "Bookmarked",
+      count: 13,
+      icon: "/icons/mobile.svg"
+    },
+    {
+      name: "Favourite",
+      count: 14,
+      icon: "/icons/star.svg"
+    },
+    {
+      name: "Trash",
+      count: 12,
+      icon: "/icons/trash.svg"
+    },
+    {
+      name: "Unassigned",
+      count: 78,
+      icon: "/icons/question.svg"
+    }
+  ];
 
-    return (
-        <div className="w-[20%] min-w-[100px] bg-light-cream text-white h-screen flex flex-col">
-            <div className="flex items-center justify-between px-4 py-10">
-                <h1 className="text-3xl text-mindful-gray-80 font-semibold">Chats</h1>
-                <Image src="/icons/search.svg" alt="Plus icon" width={24} height={24} className='cursor-pointer w-8 h-8' />
+  return (
+    <div className="w-[20%] min-w-[100px] bg-light-cream text-white h-screen flex flex-col">
+      <div className="flex items-center justify-between px-4 py-10">
+        <h1 className="text-3xl text-mindful-gray-80 font-semibold">Chats</h1>
+        <Image src="/icons/search.svg" alt="Plus icon" width={24} height={24} className='cursor-pointer w-8 h-8' />
+      </div>
+      <div className='text-xs text-slate-800 flex justify-center mt-6 mb-3'>
+        <span>All Conversations</span>
+        <span>&nbsp;·&nbsp;</span>
+        <span>345 Total</span>
+      </div>
+      <ul>
+        {items.map(({ name, count, icon }, index) => (
+          <li
+            key={index}
+            onClick={() => setSelectedItem(name)}
+            className={`flex px-4 py-8 text-mindful-gray-80 font-semibold border-b border-b-mindful-gray-40 cursor-pointer justify-between ${selectedItem === name ? 'bg-empathy-orange-10 border-r-4 border-r-empathy-orange-40' : 'hover:bg-empathy-orange-10'}`}
+          >
+            <div className="flex items-center">
+              <Image src={icon} alt={`${name} icon`} width={24} height={24} className="inline-block mr-3" />
+              {name}
             </div>
-            <div className='text-xs text-slate-800 flex justify-center mt-6 mb-3'>
-                <span>All Conversations</span>
-                <span>&nbsp;·&nbsp;</span>
-                <span>345 Total</span>
+            <div className={`${selectedItem === name ? 'text-empathy-orange-40' : 'text-mindful-gray-40'}`}>
+              {count}
             </div>
-            <ul>
-                {items.map(({ name, count, icon }, index) => (
-                    <li
-                        key={index}
-                        onClick={() => setSelectedItem(name)}
-                        className={`flex px-4 py-8 text-mindful-gray-80 font-semibold border-b border-b-mindful-gray-40 cursor-pointer justify-between ${selectedItem === name ? 'bg-empathy-orange-10 border-r-4 border-r-empathy-orange-40' : 'hover:bg-empathy-orange-10'}`}
-                    >
-                        <div className="flex items-center">
-                            <Image src={icon} alt={`${name} icon`} width={24} height={24} className="inline-block mr-3" />
-                            {name}
-                        </div>
-                        <div className={`${selectedItem === name ? 'text-empathy-orange-40' : 'text-mindful-gray-40'}`}>
-                            {count}
-                        </div>
-                    </li>
-                ))}
-            </ul>
-            <div className="mt-auto mb-4 ml-3 flex items-center space-x-3">
-                <Image src="" alt="User Name" width={48} height={48} className="rounded-full border-2 border-gray-600" />
-                <span className='text-mindful-gray-80 font-semibold'>User Name</span>
-            </div>
-        </div>
-    );
+          </li>
+        ))}
+      </ul>
+      <div className="mt-auto mb-4 ml-3 flex items-center space-x-3">
+        <Image src="" alt="User Name" width={48} height={48} className="rounded-full border-2 border-gray-600" />
+        <span className='text-mindful-gray-80 font-semibold'>User Name</span>
+      </div>
+    </div>
+  );
 }
 
 export default Chats;
--- frontend/src/app/components/sidebars/Topics.jsx
+++ frontend/src/app/components/sidebars/Topics.jsx
@@ -1,25 +1,53 @@
 import React from 'react'
 
-const ChatTopics = ["Mental Health", "Stress Anxiety", "Status Anxiety"]
+import Image from 'next/image';
+
+const ChatTopics = [
+  {
+    name: "Mental Healthhhhh",
+    icon: "/icons/avatar-0.svg"
+  },
+  {
+    name: "I don't wanna live forever",
+    icon: "/icons/avatar-1.svg"
+  },
+  {
+    name: "I just wanna keep calling your name",
+    icon: "/icons/avatar-2.svg"
+  }
+]
 
 function Topics() {
-    return (
-        <div className="w-[20%] min-w-[100px] bg-gray-800 text-white h-screen flex flex-col p-4">
-            <h1 className="text-xl mb-6">Chats</h1>
-            <ul className='list-none'>
-                {
-                    ChatTopics.map((topic, index) => (
-                        <li className="mb-2 hover:bg-gray-700 px-2 py-1 rounded" key={index}>
-                            {topic}
-                        </li>
-                    ))
-                }
-            </ul>
-            <div className="mt-auto mb-2 flex items-center space-x-3">
-                <button>Add a new conversation</button>
+  return (
+    <div className="w-[20%] min-w-[100px] bg-[#F7F4F2] h-screen flex flex-col">
+      <div className="flex flex-row items-center px-4 py-10 gap-4">
+        <Image src="/icons/chat-double.svg" alt="Chat Double icon" width={32} height={32} />
+        <p className="text-[#4B3425] text-3xl font-bold">Topics</p>
+      </div>
+      {ChatTopics.map(({ name, icon }, index) => (
+        <div className="flex flex-row items-center gap-4 p-4 w-full" key={index}>
+          <Image src={icon} alt="Plus icon" width={64} height={64} />
+          <div className="flex flex-col gap-1.5 w-full">
+            <button className="text-[#3D4A26] text-lg w-[85%] font-bold overflow-hidden whitespace-nowrap text-ellipsis text-left">{name}</button>
+            <div className="flex flex-row gap-2">
+              <button className="flex flex-row gap-1 items-center">
+                <Image src="/icons/share.svg" alt="Share icon" width={20} height={20} />
+                <div className="text-[#1F160F7A] text-sm font-semibold">Share</div>
+              </button>
+              <button className="flex flex-row gap-1 items-center">
+                <Image src="/icons/layer-add.svg" alt="Add icon" width={20} height={20} />
+                <div className="text-[#1F160F7A] text-sm font-semibold">Add</div>
+              </button>
+              <button className="flex flex-row gap-1 items-center">
+                <Image src="/icons/delete.svg" alt="Delete icon" width={20} height={20} />
+                <div className="text-[#1F160F7A] text-sm font-semibold">Delete</div>
+              </button>
             </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default Topics
