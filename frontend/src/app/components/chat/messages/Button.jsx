a84b21fef586634cc278f70cfe1e70484b4e4bc2
Malavika Harikumar <malavikaha02@gmail.com>
2023-10-04 10:16:06 +0530

    added button component

---
+++ frontend/src/app/components/chat/messages/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button className='rounded-custom bg-white text-mindful-gray-80 m-3'>
+      Some text
+    </button>
+  )
+}
+
+export default Button
