274768e7fa6be40ad7f702d0a4db8e068e37ffc0
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2023-10-16 22:42:02 +0530

    Fix stuff

--- frontend/src/app/components/chat/Header.jsx
+++ frontend/src/app/components/chat/Header.jsx
@@ -7,8 +7,6 @@ import { ApiNetworkProvider } from "@multiversx/sdk-network-providers";
 import { Transaction, TransactionPayload, TokenTransfer } from "@multiversx/sdk-core";
 import { Account } from "@multiversx/sdk-core";
 import { ProxyNetworkProvider } from "@multiversx/sdk-network-providers";
-import { ExtensionLoginButton } from '@multiversx/sdk-dapp/UI/extension/ExtensionLoginButton/ExtensionLoginButton';
-import { sendTransactions } from "@multiversx/sdk-dapp/services/transactions";
 import { Address } from '@multiversx/sdk-core';
 import axios from 'axios';
 import { SmartContract } from "@multiversx/sdk-core";
@@ -17,108 +15,45 @@ import { TransactionWatcher } from "@multiversx/sdk-core";
 import { ResultsParser } from "@multiversx/sdk-core";
 import { Code } from '@multiversx/sdk-core';
 
-
-
 function Header() {
   const [walletProvider, isConnected, setIsConnected] = useWalletProvider();
   const [account, setAccount] = useState(null);
-
-  // useEffect(() => {
-  //   console.log(account);
-
-  //   if (!walletProvider || !apiNetworkProvider) {
-  //     return;
-  //   }
-
-  //   const fetchAccountData = async () => {
-  //     if (!walletProvider.account || !walletProvider.account.address) {
-  //       return;
-  //     }
-
-  //     const _account = new Account(walletProvider.account.address);
-  //     const _accountOnNetwork = await apiNetworkProvider.getAccount(walletProvider.account.address);
-  //     if (_accountOnNetwork) {
-  //       _account.update(_accountOnNetwork);
-  //       setAccount(_account);
-  //     }
-  //   };
-
-  //   fetchAccountData();
-  // }, [walletProvider, apiNetworkProvider]);
 
   const connectWallet = async () => {
     const proxyNetworkProvider = new ProxyNetworkProvider("https://devnet-gateway.multiversx.com");
     const apiNetworkProvider = new ApiNetworkProvider("https://devnet-api.multiversx.com");
 
     try {
-      const address = await walletProvider.login();
-      // const alice = new Account(address);
-      const aliceOnNetwork = await apiNetworkProvider.getAccount(Address.fromBech32(address));
-      // substituting address directly in string format doesn't work either
-      console.log(aliceOnNetwork);
+      const deployerAddress = await walletProvider.login();
+      const deployerOnNetwork = await apiNetworkProvider.getAccount(Address.fromBech32(deployerAddress));
+      const contract = new SmartContract();
 
-      // const tx = new Transaction({
-      //   data: new TransactionPayload("helloWorld"),
-      //   gasLimit: 70000,
-      //   sender: Address.fromBech32(address),
-      //   receiver: Address.fromBech32('erd17enrv0r7d8gqscplkw4ymec6j246ze54tklrfq4lshwyndpl6lcq4gjtx8'),
-      //   value: TokenTransfer.egldFromAmount(1),
-      //   chainID: "D"
-      // });
-
-      // tx.setNonce(aliceOnNetwork.nonce + 1);
-
-      // const _tx = await walletProvider.signTransaction(tx);
-      // let txHash = await proxyNetworkProvider.sendTransaction(_tx); 
-      // console.log(txHash);
-
-      let response = await axios.get("http://localhost:3000/api");
-      console.log(response)
-
+      const response = await axios.get("http://localhost:3000/api");
       const buffer = Buffer.from(response.data.buffer);
       const code = Code.fromBuffer(buffer);
-
-      const deployerAddress = address;
-
-      let contract = new SmartContract();
 
       const deployTransaction = contract.deploy({
         deployer: Address.fromBech32(deployerAddress),
         code: code,
-        codeMetadata: new CodeMetadata(/* set the parameters accordingly */),
-        initArguments: [/* set the initial arguments, if any */],
+        codeMetadata: new CodeMetadata(),
+        initArguments: [],
         gasLimit: 20000000,
         chainID: "D"
       });
-
-      deployTransaction.setNonce(aliceOnNetwork.nonce + 1);
-
-      let contractAddress = SmartContract.computeAddress(deployTransaction.getSender(), deployTransaction.getNonce());
-      console.log("Contract address:", contractAddress.bech32());
-
+      deployTransaction.setNonce(deployerOnNetwork.nonce);
+      
       const _deployTransaction = await walletProvider.signTransaction(deployTransaction);
 
       await apiNetworkProvider.sendTransaction(_deployTransaction);
       let transactionOnNetwork = await new TransactionWatcher(apiNetworkProvider).awaitCompleted(_deployTransaction);
 
-      // Currently getting Expected transaction status not reached error here
-      let { returnCode } = new ResultsParser().parseUntypedOutcome(transactionOnNetwork);
+      console.log("Transaction hash:", transactionOnNetwork);
+
+      const { returnCode } = new ResultsParser().parseUntypedOutcome(transactionOnNetwork);
       console.log("Return code:", returnCode);
     } catch (error) {
       console.error(error);
     }
-  };
-
-  const connectWallet2 = async () => {
-    console.log("Hi")
-    const { sessionId, error } = await sendTransactions({
-      transactions: [
-        {
-          value: '1000000000000000000',
-          receiver: 'erd1xjwy83a59ggvvyzu3gdq4q2dp85lqqdf9gkus6wmqupmsnwc2laqtmhsej'
-        },
-      ]
-    });
   };
 
   return (
@@ -127,8 +62,7 @@ function Header() {
       <div className="flex items-center">
         <ChainInfo connected={isConnected} />
         <UserModal />
-        <Button onClick={connectWallet} />
-        {/* <Button onClick={signTransaction} /> */}
+        {/* <Button onClick={connectWallet} /> */}
       </div>
     </div>
   );
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,39 +1,10 @@
 'use client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 import { useEffect, useState } from 'react'
 import Topics from '@/app/components/sidebars/Topics'
 import ChatWindow from './components/chat/ChatWindow'
 import { useRouter } from 'next/navigation'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useChatStore } from '@/store/store'
-
-import { AxiosInterceptorContext } from '@multiversx/sdk-dapp/wrappers/AxiosInterceptorContext/AxiosInterceptorContext';
-import { DappProvider } from '@multiversx/sdk-dapp/wrappers/DappProvider/DappProvider';
-import { TransactionsToastList } from '@multiversx/sdk-dapp/UI/TransactionsToastList/TransactionsToastList';
-import { NotificationModal } from '@multiversx/sdk-dapp/UI/NotificationModal/NotificationModal';
-import { SignTransactionsModals } from '@multiversx/sdk-dapp/UI/SignTransactionsModals/SignTransactionsModals';
-import  { EnvironmentsEnum } from '@multiversx/sdk-dapp/types/enums.types';
-const AppContent = () => {
-  const walletConnectV2ProjectId = '9b1a9564f91cb659ffe21b73d5c4e2d8';
-  const apiTimeout = 6000;
-  const transactionSize = 10;
-  const nativeAuth = true;
-
-  return (
-    <Router>
-      <DappProvider
-        environment={EnvironmentsEnum.devnet}
-      >
-        <AxiosInterceptorContext.Listener>
-            <TransactionsToastList />
-            <NotificationModal />
-            <SignTransactionsModals />
-        </AxiosInterceptorContext.Listener>
-      </DappProvider>
-    </Router>
-  );
-};
 
 export default function Home() {
   const supabase = createClientComponentClient()
@@ -58,19 +29,11 @@ export default function Home() {
     }
     fetchUser()
   }, [])
-  const API_URL = 'https://testnet-template-api.multiversx.com';
-  const sampleAuthenticatedDomains = [API_URL];
+  
   return (
-    <AxiosInterceptorContext.Provider>
-      <AxiosInterceptorContext.Interceptor
-        authenticatedDomanis={sampleAuthenticatedDomains}
-      >
-        <AppContent />
-      <div className="flex flex-row">
-        <Topics />
-        <ChatWindow />
-      </div>
-      </AxiosInterceptorContext.Interceptor>
-    </AxiosInterceptorContext.Provider>
+    <div className="flex flex-row">
+      <Topics />
+      <ChatWindow />
+    </div>
   )
 }
