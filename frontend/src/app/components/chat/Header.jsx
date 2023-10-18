0e806feec87434aaad5e6af8a96574eadd26ac7f
Malavika Harikumar <malavikaha02@gmail.com>
2023-10-18 11:05:49 +0530

    Added function to interact with erc20 contract

--- frontend/src/app/components/chat/Header.jsx
+++ frontend/src/app/components/chat/Header.jsx
@@ -4,57 +4,64 @@ import UserModal from '../modals/UserModal';
 import Button from './messages/Button';
 import useWalletProvider from '@/hooks/useWalletProvider';
 import { ApiNetworkProvider } from "@multiversx/sdk-network-providers";
-import { Transaction, TransactionPayload, TokenTransfer } from "@multiversx/sdk-core";
-import { Account } from "@multiversx/sdk-core";
-import { ProxyNetworkProvider } from "@multiversx/sdk-network-providers";
-import { Address } from '@multiversx/sdk-core';
+import { Address, AddressValue, BigIntValue } from '@multiversx/sdk-core';
 import axios from 'axios';
 import { SmartContract } from "@multiversx/sdk-core";
-import { CodeMetadata } from "@multiversx/sdk-core";
-import { TransactionWatcher } from "@multiversx/sdk-core";
 import { ResultsParser } from "@multiversx/sdk-core";
-import { Code } from '@multiversx/sdk-core';
+import { AbiRegistry } from '@multiversx/sdk-core';
+import { TokenTransfer } from '@multiversx/sdk-core';
 
 function Header() {
   const [walletProvider, isConnected, setIsConnected] = useWalletProvider();
-  const [account, setAccount] = useState(null);
 
-  const connectWallet = async () => {
-    const proxyNetworkProvider = new ProxyNetworkProvider("https://devnet-gateway.multiversx.com");
-    const apiNetworkProvider = new ApiNetworkProvider("https://devnet-api.multiversx.com");
+  async function queryContract(contractAddressBech32, abiUrl) {
+    // Load the ABI from an URL
+    const response = await axios.get(`http://localhost:3000/api/abi?abiUrl=${abiUrl}`);
+    const abiRegistry = AbiRegistry.create(response.data.data);
+    const networkProvider = new ApiNetworkProvider("https://devnet-api.multiversx.com");
+    const deployerAddress = await walletProvider.login();
+    const recipient = Address.fromBech32("erd17enrv0r7d8gqscplkw4ymec6j246ze54tklrfq4lshwyndpl6lcq4gjtx8");
+    console.log(abiRegistry)
+  
+    // Create the SmartContract instance
+    const contractAddress = Address.fromBech32(contractAddressBech32);
+    const existingContract = new SmartContract({ address: contractAddress, abi: abiRegistry});
+  
+    // Create the query
+    const query = existingContract.createQuery({
+      func: "balanceOf",
+      args: [new AddressValue(recipient)]
+    });
+  
+    const queryResponse = await networkProvider.queryContract(query);
+  
+    const getEndpoint = abiRegistry.getEndpoint("balanceOf");
+    const { values } = new ResultsParser().parseQueryResponse(queryResponse, getEndpoint);
+    
+    // const deployerOnNetwork = await networkProvider.getAccount(Address.fromBech32(deployerAddress));
+    // console.log(deployerAddress)
+    // let tx3 = existingContract.methods.transfer([new AddressValue(recipient), new BigIntValue(10)])
+    //   .withSender(Address.fromBech32(deployerAddress))
+    //   .withNonce(deployerOnNetwork.nonce)
+    //   .withGasLimit(20000000)
+    //   .withChainID("D")
+    //   .buildTransaction();
 
-    try {
-      const deployerAddress = await walletProvider.login();
-      const deployerOnNetwork = await apiNetworkProvider.getAccount(Address.fromBech32(deployerAddress));
-      const contract = new SmartContract();
+    // tx3 = await walletProvider.signTransaction(tx3);
+    // const txHash3 = await networkProvider.sendTransaction(tx3);
+  
+    // console.log("tx3", tx3);
+  
+    return values[0].valueOf().toFixed(0);
+  }
 
-      const response = await axios.get("http://localhost:3000/api");
-      const buffer = Buffer.from(response.data.buffer);
-      const code = Code.fromBuffer(buffer);
-
-      const deployTransaction = contract.deploy({
-        deployer: Address.fromBech32(deployerAddress),
-        code: code,
-        codeMetadata: new CodeMetadata(),
-        initArguments: [],
-        gasLimit: 20000000,
-        chainID: "D"
-      });
-      deployTransaction.setNonce(deployerOnNetwork.nonce);
-      
-      const _deployTransaction = await walletProvider.signTransaction(deployTransaction);
-
-      await apiNetworkProvider.sendTransaction(_deployTransaction);
-      let transactionOnNetwork = await new TransactionWatcher(apiNetworkProvider).awaitCompleted(_deployTransaction);
-
-      console.log("Transaction hash:", transactionOnNetwork);
-
-      const { returnCode } = new ResultsParser().parseUntypedOutcome(transactionOnNetwork);
-      console.log("Return code:", returnCode);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const createTransaction = () => {
+    const contractAddress = "erd1qqqqqqqqqqqqqpgqdjfrylk03uvwlqc2t3l8ymwelev3zmw3wdtqqy0fzc";
+    const abiEndpoint = "https://github.com/CommanderAstern/encode-multiversX/raw/sdk-2/frontend/public/contract/erc20.abi.json";
+    queryContract(contractAddress, abiEndpoint).then(result => {
+      console.log("Claimable rewards:", result);
+    });
+  }
 
   return (
     <div className="flex items-center justify-between px-4 py-7 border-b border-b-mindful-gray-40">
@@ -62,6 +69,7 @@ function Header() {
       <div className="flex items-center">
         <ChainInfo connected={isConnected} />
         <UserModal />
+        <Button onClick={createTransaction} label={"Query Contract"} />
       </div>
     </div>
   );
